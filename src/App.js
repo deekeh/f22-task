3c27f2d9821dfb5b1186a336125bf5792e7c9082
DK <darshankhot@programmer.net>
2021-02-05 18:41:08 +0530

    added dummy dashboard with router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import "./App.css";
+import { HashRouter as Router, Route } from "react-router-dom";
 
+// components
 import Login from "./components/Login";
+import Dashboard from "./components/dashboard/Dashboard";
 
 // bootstrap
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -8,7 +11,11 @@ import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
   return (
     <div className="App">
-      <Login />
+      <Router>
+        {/* <Login /> */}
+        <Route path="/" exact component={Login} />
+        <Route path="/dashboard" component={Dashboard} />
+      </Router>
     </div>
   );
 }
