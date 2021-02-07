0d154b4ef202fcdc870fe54a87dc8f7bb40930e6
DK <darshankhot@programmer.net>
2021-02-07 12:49:20 +0530

    added edit bar & detail-chat bar[incomplete]

--- src/components/dashboard/DetailChat.js
+++ src/components/dashboard/DetailChat.js
@@ -1,5 +1,108 @@
 const DetailChat = () => {
-  return <div id="dashboard-detail-chat" className="apps-item"></div>;
+  return (
+    <div id="dashboard-detail-chat">
+      <div id="dc-header">
+        <div id="dc-name" className="ml-4 p-3">
+          <b>Matt Thompson</b> is typing...
+        </div>
+        <div id="dc-icons">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="#3c3c3c"
+            className="bi bi-star"
+            viewBox="0 0 16 16"
+            id="dc-star"
+          >
+            <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.523-3.356c.329-.314.158-.888-.283-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767l-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288l1.847-3.658 1.846 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.564.564 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z" />
+          </svg>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            className="bi bi-telephone"
+            viewBox="0 0 16 16"
+            id="dc-voice"
+          >
+            <path d="M3.654 1.328a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.568 17.568 0 0 0 4.168 6.608 17.569 17.569 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.678.678 0 0 0-.58-.122l-2.19.547a1.745 1.745 0 0 1-1.657-.459L5.482 8.062a1.745 1.745 0 0 1-.46-1.657l.548-2.19a.678.678 0 0 0-.122-.58L3.654 1.328zM1.884.511a1.745 1.745 0 0 1 2.612.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z" />
+          </svg>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            className="bi bi-camera-video"
+            viewBox="0 0 16 16"
+            id="dc-video"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M0 5a2 2 0 0 1 2-2h7.5a2 2 0 0 1 1.983 1.738l3.11-1.382A1 1 0 0 1 16 4.269v7.462a1 1 0 0 1-1.406.913l-3.111-1.382A2 2 0 0 1 9.5 13H2a2 2 0 0 1-2-2V5zm11.5 5.175l3.5 1.556V4.269l-3.5 1.556v4.35zM2 4a1 1 0 0 0-1 1v6a1 1 0 0 0 1 1h7.5a1 1 0 0 0 1-1V5a1 1 0 0 0-1-1H2z"
+            />
+          </svg>
+        </div>
+      </div>
+
+      <div id="dc-content" className>
+        <div className="dc-messages"></div>
+        <div className="dc-reply px-3">
+          <div id="dc-link">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="32"
+              height="32"
+              fill="currentColor"
+              className="bi bi-link-45deg"
+              viewBox="0 0 16 16"
+            >
+              <path d="M4.715 6.542L3.343 7.914a3 3 0 1 0 4.243 4.243l1.828-1.829A3 3 0 0 0 8.586 5.5L8 6.086a1.001 1.001 0 0 0-.154.199 2 2 0 0 1 .861 3.337L6.88 11.45a2 2 0 1 1-2.83-2.83l.793-.792a4.018 4.018 0 0 1-.128-1.287z" />
+              <path d="M6.586 4.672A3 3 0 0 0 7.414 9.5l.775-.776a2 2 0 0 1-.896-3.346L9.12 3.55a2 2 0 0 1 2.83 2.83l-.793.792c.112.42.155.855.128 1.287l1.372-1.372a3 3 0 0 0-4.243-4.243L6.586 4.672z" />
+            </svg>
+          </div>
+          <div id="dc-text" className="w-100">
+            <input
+              className="w-100 p-1 rounded"
+              type="text"
+              placeholder="Type your message..."
+            />
+          </div>
+          <div id="dc-emoticon" className="mx-3">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="32"
+              height="32"
+              fill="currentColor"
+              className="bi bi-emoji-laughing"
+              viewBox="0 0 16 16"
+            >
+              <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+              <path d="M12.331 9.5a1 1 0 0 1 0 1A4.998 4.998 0 0 1 8 13a4.998 4.998 0 0 1-4.33-2.5A1 1 0 0 1 4.535 9h6.93a1 1 0 0 1 .866.5zM7 6.5c0 .828-.448 0-1 0s-1 .828-1 0S5.448 5 6 5s1 .672 1 1.5zm4 0c0 .828-.448 0-1 0s-1 .828-1 0S9.448 5 10 5s1 .672 1 1.5z" />
+            </svg>
+          </div>
+          <div id="dc-send" className="rounded-circle p-3">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="icon icon-tabler icon-tabler-send"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              stroke-width="2"
+              stroke="#ffffff"
+              fill="none"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            >
+              <path stroke="none" d="M0 0h24v24H0z" />
+              <line x1="10" y1="14" x2="21" y2="3" />
+              <path d="M21 3L14.5 21a.55 .55 0 0 1 -1 0L10 14L3 10.5a.55 .55 0 0 1 0 -1L21 3" />
+            </svg>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default DetailChat;
--- src/components/dashboard/Edit.js
+++ src/components/dashboard/Edit.js
@@ -1,5 +1,102 @@
+import gmail from "./img/gmail.png";
+
+import { Dropdown } from "react-bootstrap";
+
 const Edit = () => {
-  return <div id="dashboard-edit" className="apps-item"></div>;
+  return (
+    <div id="dashboard-edit">
+      <div id="e-header">
+        <div id="e-notif" className="ml-3">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            className="bi bi-bell"
+            viewBox="0 0 16 16"
+          >
+            <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2zM8 1.918l-.797.161A4.002 4.002 0 0 0 4 6c0 .628-.134 2.197-.459 3.742-.16.767-.376 1.566-.663 2.258h10.244c-.287-.692-.502-1.49-.663-2.258C12.134 8.197 12 6.628 12 6a4.002 4.002 0 0 0-3.203-3.92L8 1.917zM14.22 12c.223.447.481.801.78 1H1c.299-.199.557-.553.78-1C2.68 10.2 3 6.88 3 6c0-2.42 1.72-4.44 4.005-4.901a1 1 0 1 1 1.99 0A5.002 5.002 0 0 1 13 6c0 .88.32 4.2 1.22 6z" />
+          </svg>
+        </div>
+        <div id="e-name" className="mr-3">
+          <Dropdown>
+            <Dropdown.Toggle className="bg-light text-secondary border-secondary">
+              Johan Horn
+            </Dropdown.Toggle>
+            <Dropdown.Menu>
+              <Dropdown.Item>Something 1</Dropdown.Item>
+              <Dropdown.Item>Something 2</Dropdown.Item>
+              <Dropdown.Item>Something 3</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        </div>
+      </div>
+
+      <div id="e-body">
+        <div id="e-apps">
+          <div id="e-gmail" className="ml-4">
+            <img src={gmail} alt="gmail-logo" width="20" />
+          </div>
+          <div id="e-menu" className="mr-4">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="20"
+              height="20"
+              fill="currentColor"
+              className="bi bi-three-dots"
+              viewBox="0 0 16 16"
+            >
+              <path d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z" />
+            </svg>
+          </div>
+        </div>
+
+        <div id="e-profile" class="px-4 mt-4">
+          <img
+            src="https://loremflickr.com/g/240/240/portrait"
+            alt="profile-pic"
+            width="150"
+          />
+          <h5 className="mt-3">Matt Thompson</h5>
+          <small>Cape Town, RSA</small>
+        </div>
+        <hr className="mx-2" />
+
+        <div id="e-about" className="px-2">
+          <div className="e-about-line mb-1">
+            <span className="line-label">Nickname:</span>
+            <span className="line-data">
+              <b>Triple T</b>
+            </span>
+          </div>
+          <div className="e-about-line mb-1">
+            <span className="line-label">Tel:</span>
+            <span className="line-data">
+              <b>021 88 237 8890</b>
+            </span>
+          </div>
+          <div className="e-about-line mb-1">
+            <span className="line-label">Date of Birth:</span>
+            <span className="line-data">
+              <b>June 20th 1983</b>
+            </span>
+          </div>
+          <div className="e-about-line mb-1">
+            <span className="line-label">Gender:</span>
+            <span className="line-data">
+              <b>Male</b>
+            </span>
+          </div>
+          <div className="e-about-line mb-1">
+            <span className="line-label">Language:</span>
+            <span className="line-data">
+              <b>English</b>
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Edit;
